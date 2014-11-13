9d392fa61e1b62543a0e18184d66db5815a012a7
piers55 <apiers55@gmail.com>
2014-11-12 19:25:27 -0600

    lightbox y correcciones index

--- js/plugins.js
+++ js/plugins.js
@@ -119,3 +119,318 @@ function(a){"use strict";a.extend(a.fn.cycle.defaults,{tmplRegex:"{{((.)?.*?)}}"
 !function(l){var e="mmenu",s="labels";l[e].addons[s]={_init:function(e){var n=this.opts[s];this.__refactorClass(l("li",this.$menu),this.conf.classNames[s].collapsed,"collapsed"),n.collapse&&l("."+a.label,e).each(function(){var e=l(this),s=e.nextUntil("."+a.label,"."+a.collapsed);s.length&&(e.children("."+a.subopen).length||(e.wrapInner("<span />"),e.prepend('<a href="#" class="'+a.subopen+" "+a.fullsubopen+'" />')))})},_setup:function(){var a=this.opts[s];"boolean"==typeof a&&(a={collapse:a}),"object"!=typeof a&&(a={}),a=l.extend(!0,{},l[e].defaults[s],a),this.opts[s]=a},_add:function(){a=l[e]._c,n=l[e]._d,o=l[e]._e,a.add("collapsed uncollapsed"),t=l[e].glbl},_clickAnchor:function(l){var e=l.parent();if(e.is("."+a.label)){var s=e.nextUntil("."+a.label,"."+a.collapsed);return e.toggleClass(a.opened),s[e.hasClass(a.opened)?"addClass":"removeClass"](a.uncollapsed),!0}return!1}},l[e].defaults[s]={collapse:!1},l[e].configuration.classNames[s]={collapsed:"Collapsed"};var a,n,o,t}(jQuery);
 !function(e){function s(e){switch(e){case 9:case 16:case 17:case 18:case 37:case 38:case 39:case 40:return!0}return!1}var n="mmenu",t="searchfield";e[n].addons[t]={_init:function(n){var i=this,l=this.opts[t],d=this.conf[t];if(l.add){switch(l.addTo){case"menu":var c=this.$menu;break;case"panels":var c=n;break;default:var c=e(l.addTo,this.$menu).filter("."+a.panel)}c.length&&c.each(function(){var s=e(this),n=s.is("."+a.menu)?d.form?"form":"div":"li";if(!s.children(n+"."+a.search).length){if(s.is("."+a.menu))var t=i.$menu,r="prependTo";else var t=s.children().first(),r=t.is("."+a.subtitle)?"insertAfter":"insertBefore";var o=e("<"+n+' class="'+a.search+'" />');if("form"==n&&"object"==typeof d.form)for(var c in d.form)o.attr(c,d.form[c]);o.append('<input placeholder="'+l.placeholder+'" type="text" autocomplete="off" />'),o[r](t)}l.noResults&&(s.is("."+a.menu)&&(s=s.children("."+a.panel).first()),n=s.is("."+a.list)?"li":"div",s.children(n+"."+a.noresultsmsg).length||e("<"+n+' class="'+a.noresultsmsg+'" />').html(l.noResults).appendTo(s))})}if(this.$menu.children("."+a.search).length&&this.$menu.addClass(a.hassearch),l.search){var h=e("."+a.search,this.$menu);h.length&&h.each(function(){var n=e(this);if("menu"==l.addTo)var t=e("."+a.panel,i.$menu),d=i.$menu;else var t=n.closest("."+a.panel),d=t;var c=n.children("input"),h=i.__findAddBack(t,"."+a.list).children("li"),u=h.filter("."+a.label),f=h.not("."+a.subtitle).not("."+a.label).not("."+a.search).not("."+a.noresultsmsg),p="> a";l.showLinksOnly||(p+=", > span"),c.off(o.keyup+" "+o.change).on(o.keyup,function(e){s(e.keyCode)||n.trigger(o.search)}).on(o.change,function(){n.trigger(o.search)}),n.off(o.reset+" "+o.search).on(o.reset+" "+o.search,function(e){e.stopPropagation()}).on(o.reset,function(){n.trigger(o.search,[""])}).on(o.search,function(s,n){"string"==typeof n?c.val(n):n=c.val(),n=n.toLowerCase(),t.scrollTop(0),f.add(u).addClass(a.hidden),f.each(function(){var s=e(this);e(p,s).text().toLowerCase().indexOf(n)>-1&&s.add(s.prevAll("."+a.label).first()).removeClass(a.hidden)}),e(t.get().reverse()).each(function(s){var n=e(this),t=n.data(r.parent);if(t){var d=n.add(n.find("> ."+a.list)).find("> li").not("."+a.subtitle).not("."+a.search).not("."+a.noresultsmsg).not("."+a.label).not("."+a.hidden);d.length?t.removeClass(a.hidden).removeClass(a.nosubresults).prevAll("."+a.label).first().removeClass(a.hidden):"menu"==l.addTo&&(n.hasClass(a.opened)&&setTimeout(function(){t.trigger(o.open)},1.5*(s+1)*i.conf.openingInterval),t.addClass(a.nosubresults))}}),d[f.not("."+a.hidden).length?"removeClass":"addClass"](a.noresults),i._update()})})}},_setup:function(){var s=this.opts[t];this.conf[t],"boolean"==typeof s&&(s={add:s,search:s}),"object"!=typeof s&&(s={}),s=e.extend(!0,{},e[n].defaults[t],s),"boolean"!=typeof s.showLinksOnly&&(s.showLinksOnly="menu"==s.addTo),this.opts[t]=s},_add:function(){a=e[n]._c,r=e[n]._d,o=e[n]._e,a.add("search hassearch noresultsmsg noresults nosubresults"),o.add("search reset change"),i=e[n].glbl}},e[n].defaults[t]={add:!1,addTo:"menu",search:!1,placeholder:"Search",noResults:"No results found."},e[n].configuration[t]={form:!1};var a,r,o,i}(jQuery);
 !function(e){var t="mmenu",s="toggles";e[t].addons[s]={_init:function(t){var a=this;this.opts[s],this.conf[s],this.__refactorClass(e("input",t),this.conf.classNames[s].toggle,"toggle"),this.__refactorClass(e("input",t),this.conf.classNames[s].check,"check"),e("input."+c.toggle+", input."+c.check,t).each(function(){var t=e(this),s=t.closest("li"),l=t.hasClass(c.toggle)?"toggle":"check",n=t.attr("id")||a.__getUniqueId();s.children('label[for="'+n+'"]').length||(t.attr("id",n),s.prepend(t),e('<label for="'+n+'" class="'+c[l]+'"></label>').insertBefore(s.children("a, span").last()))})},_setup:function(){},_add:function(){c=e[t]._c,a=e[t]._d,l=e[t]._e,c.add("toggle check"),n=e[t].glbl}},e[t].defaults[s]={},e[t].configuration.classNames[s]={toggle:"Toggle",check:"Check"};var c,a,l,n}(jQuery);
+
+
+
+
+
+
+/* LIGHTBOX */
+
+;( function( $, window, document, undefined )
+{
+	'use strict';
+
+	var cssTransitionSupport = function()
+		{
+			var s = document.body || document.documentElement, s = s.style;
+			if( s.WebkitTransition == '' ) return '-webkit-';
+			if( s.MozTransition == '' ) return '-moz-';
+			if( s.OTransition == '' ) return '-o-';
+			if( s.transition == '' ) return '';
+			return false;
+		},
+
+		isCssTransitionSupport = cssTransitionSupport() === false ? false : true,
+
+		cssTransitionTranslateX = function( element, positionX, speed )
+		{
+			var options = {}, prefix = cssTransitionSupport();
+			options[ prefix + 'transform' ]	 = 'translateX(' + positionX + ')';
+			options[ prefix + 'transition' ] = prefix + 'transform ' + speed + 's linear';
+			element.css( options );
+		},
+
+		hasTouch	= ( 'ontouchstart' in window ),
+		hasPointers = window.navigator.pointerEnabled || window.navigator.msPointerEnabled,
+		wasTouched	= function( event )
+		{
+			if( hasTouch )
+				return true;
+
+			if( !hasPointers || typeof event === 'undefined' || typeof event.pointerType === 'undefined' )
+				return false;
+
+			if( typeof event.MSPOINTER_TYPE_MOUSE !== 'undefined' )
+			{
+				if( event.MSPOINTER_TYPE_MOUSE != event.pointerType )
+					return true;
+			}
+			else
+				if( event.pointerType != 'mouse' )
+					return true;
+
+			return false;
+		};
+
+	$.fn.imageLightbox = function( options )
+	{
+		var options	   = $.extend(
+						 {
+						 	selector:		'id="imagelightbox"',
+						 	allowedTypes:	'png|jpg|jpeg|gif',
+						 	animationSpeed:	250,
+						 	preloadNext:	true,
+						 	enableKeyboard:	true,
+						 	quitOnEnd:		false,
+						 	quitOnImgClick: false,
+						 	quitOnDocClick: true,
+						 	onStart:		false,
+						 	onEnd:			false,
+						 	onLoadStart:	false,
+						 	onLoadEnd:		false
+						 },
+						 options ),
+
+			targets		= $([]),
+			target		= $(),
+			image		= $(),
+			imageWidth	= 0,
+			imageHeight = 0,
+			swipeDiff	= 0,
+			inProgress	= false,
+
+			isTargetValid = function( element )
+			{
+				return $( element ).prop( 'tagName' ).toLowerCase() == 'a' && ( new RegExp( '\.(' + options.allowedTypes + ')$', 'i' ) ).test( $( element ).attr( 'href' ) );
+			},
+
+			setImage = function()
+			{
+				if( !image.length ) return true;
+
+				var screenWidth	 = $( window ).width() * 0.8,
+					screenHeight = $( window ).height() * 0.9,
+					tmpImage 	 = new Image();
+
+				tmpImage.src	= image.attr( 'src' );
+				tmpImage.onload = function()
+				{
+					imageWidth	 = tmpImage.width;
+					imageHeight	 = tmpImage.height;
+
+					if( imageWidth > screenWidth || imageHeight > screenHeight )
+					{
+						var ratio	 = imageWidth / imageHeight > screenWidth / screenHeight ? imageWidth / screenWidth : imageHeight / screenHeight;
+						imageWidth	/= ratio;
+						imageHeight	/= ratio;
+					}
+
+					image.css(
+					{
+						'width':  imageWidth + 'px',
+						'height': imageHeight + 'px',
+						'top':    ( $( window ).height() - imageHeight ) / 2 + 'px',
+						'left':   ( $( window ).width() - imageWidth ) / 2 + 'px'
+					});
+				};
+			},
+
+			loadImage = function( direction )
+			{
+				if( inProgress ) return false;
+
+				direction = typeof direction === 'undefined' ? false : direction == 'left' ? 1 : -1;
+
+				if( image.length )
+				{
+					if( direction !== false && ( targets.length < 2 || ( options.quitOnEnd === true && ( ( direction === -1 && targets.index( target ) == 0 ) || ( direction === 1 && targets.index( target ) == targets.length - 1 ) ) ) ) )
+					{
+						quitLightbox();
+						return false;
+					}
+					var params = { 'opacity': 0 };
+					if( isCssTransitionSupport ) cssTransitionTranslateX( image, ( 100 * direction ) - swipeDiff + 'px', options.animationSpeed / 1000 );
+					else params.left = parseInt( image.css( 'left' ) ) + 100 * direction + 'px';
+					image.animate( params, options.animationSpeed, function(){ removeImage(); });
+					swipeDiff = 0;
+				}
+
+				inProgress = true;
+				if( options.onLoadStart !== false ) options.onLoadStart();
+
+				setTimeout( function()
+				{
+					image = $( '<img ' + options.selector + ' />' )
+					.attr( 'src', target.attr( 'href' ) )
+					.load( function()
+					{
+						image.appendTo( 'body' );
+						setImage();
+
+						var params = { 'opacity': 1 };
+
+						image.css( 'opacity', 0 );
+						if( isCssTransitionSupport )
+						{
+							cssTransitionTranslateX( image, -100 * direction + 'px', 0 );
+							setTimeout( function(){ cssTransitionTranslateX( image, 0 + 'px', options.animationSpeed / 1000 ) }, 50 );
+						}
+						else
+						{
+							var imagePosLeft = parseInt( image.css( 'left' ) );
+							params.left = imagePosLeft + 'px';
+							image.css( 'left', imagePosLeft - 100 * direction + 'px' );
+						}
+
+						image.animate( params, options.animationSpeed, function()
+						{
+							inProgress = false;
+							if( options.onLoadEnd !== false ) options.onLoadEnd();
+						});
+						if( options.preloadNext )
+						{
+							var nextTarget = targets.eq( targets.index( target ) + 1 );
+							if( !nextTarget.length ) nextTarget = targets.eq( 0 );
+							$( '<img />' ).attr( 'src', nextTarget.attr( 'href' ) ).load();
+						}
+					})
+					.error( function()
+					{
+						if( options.onLoadEnd !== false ) options.onLoadEnd();
+					})
+
+					var swipeStart	 = 0,
+						swipeEnd	 = 0,
+						imagePosLeft = 0;
+
+					image.on( hasPointers ? 'pointerup MSPointerUp' : 'click', function( e )
+					{
+						e.preventDefault();
+						if( options.quitOnImgClick )
+						{
+							quitLightbox();
+							return false;
+						}
+						if( wasTouched( e.originalEvent ) ) return true;
+					    var posX = ( e.pageX || e.originalEvent.pageX ) - e.target.offsetLeft;
+						target = targets.eq( targets.index( target ) - ( imageWidth / 2 > posX ? 1 : -1 ) );
+						if( !target.length ) target = targets.eq( imageWidth / 2 > posX ? targets.length : 0 );
+						loadImage( imageWidth / 2 > posX ? 'left' : 'right' );
+					})
+					.on( 'touchstart pointerdown MSPointerDown', function( e )
+					{
+						if( !wasTouched( e.originalEvent ) || options.quitOnImgClick ) return true;
+						if( isCssTransitionSupport ) imagePosLeft = parseInt( image.css( 'left' ) );
+						swipeStart = e.originalEvent.pageX || e.originalEvent.touches[ 0 ].pageX;
+					})
+					.on( 'touchmove pointermove MSPointerMove', function( e )
+					{
+						if( !wasTouched( e.originalEvent ) || options.quitOnImgClick ) return true;
+						e.preventDefault();
+						swipeEnd = e.originalEvent.pageX || e.originalEvent.touches[ 0 ].pageX;
+						swipeDiff = swipeStart - swipeEnd;
+						if( isCssTransitionSupport ) cssTransitionTranslateX( image, -swipeDiff + 'px', 0 );
+						else image.css( 'left', imagePosLeft - swipeDiff + 'px' );
+					})
+					.on( 'touchend touchcancel pointerup pointercancel MSPointerUp MSPointerCancel', function( e )
+					{
+						if( !wasTouched( e.originalEvent ) || options.quitOnImgClick ) return true;
+						if( Math.abs( swipeDiff ) > 50 )
+						{
+							target = targets.eq( targets.index( target ) - ( swipeDiff < 0 ? 1 : -1 ) );
+							if( !target.length ) target = targets.eq( swipeDiff < 0 ? targets.length : 0 );
+							loadImage( swipeDiff > 0 ? 'right' : 'left' );	
+						}
+						else
+						{
+							if( isCssTransitionSupport ) cssTransitionTranslateX( image, 0 + 'px', options.animationSpeed / 1000 );
+							else image.animate({ 'left': imagePosLeft + 'px' }, options.animationSpeed / 2 );
+						}
+					});
+				}, options.animationSpeed + 100 );
+			},
+
+			removeImage = function()
+			{
+				if( !image.length ) return false;
+				image.remove();
+				image = $();
+			},
+
+			quitLightbox = function()
+			{
+				if( !image.length ) return false;
+				image.animate({ 'opacity': 0 }, options.animationSpeed, function()
+				{
+					removeImage();
+					inProgress = false;
+					if( options.onEnd !== false ) options.onEnd();
+				});
+			};
+
+		$( window ).on( 'resize', setImage );
+
+		if( options.quitOnDocClick )
+		{
+			$( document ).on( hasTouch ? 'touchend' : 'click', function( e )
+			{
+				if( image.length && !$( e.target ).is( image ) ) quitLightbox();
+			})
+		}
+
+		if( options.enableKeyboard )
+		{
+			$( document ).on( 'keyup', function( e )
+			{
+				if( !image.length ) return true;
+				e.preventDefault();
+				if( e.keyCode == 27 ) quitLightbox();
+				if( e.keyCode == 37 || e.keyCode == 39 )
+				{
+					target = targets.eq( targets.index( target ) - ( e.keyCode == 37 ? 1 : -1 ) );
+					if( !target.length ) target = targets.eq( e.keyCode == 37 ? targets.length : 0 );
+					loadImage( e.keyCode == 37 ? 'left' : 'right' );
+				}
+			});
+		}
+
+		$( document ).on( 'click', this.selector, function( e )
+		{
+			if( !isTargetValid( this ) ) return true;
+			e.preventDefault();
+			if( inProgress ) return false;
+			inProgress = false;
+			if( options.onStart !== false ) options.onStart();
+			target = $( this );
+			loadImage();
+		});
+
+		this.each( function()
+		{
+			if( !isTargetValid( this ) ) return true;
+			targets = targets.add( $( this ) );
+		});
+
+		this.switchImageLightbox = function( index )
+		{
+			var tmpTarget = targets.eq( index );
+			if( tmpTarget.length )
+			{
+				var currentIndex = targets.index( target );
+				target = tmpTarget;
+				loadImage( index < currentIndex ? 'left' : 'right' );
+			}
+			return this;
+		};
+
+		this.quitImageLightbox = function()
+		{
+			quitLightbox();
+			return this;
+		};
+
+		return this;
+	};
+	
+})( jQuery, window, document );
