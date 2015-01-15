da7530725d94b0f4b6ca7b3f6e4a961bdd6d8a9a
raul <raul@zamacona.me>
2015-01-14 19:25:31 -0600

    fucking nonce

--- themes/hacedores/inc/metaboxes.php
+++ themes/hacedores/inc/metaboxes.php
@@ -7,7 +7,7 @@
 
 	add_action('add_meta_boxes', function(){
 
-		//add_meta_box( 'detalles', 'Detalles', 'metabox_detalles', 'informacion', 'side', 'high' );
+		add_meta_box( 'detalles', 'Detalles', 'metabox_detalles', 'informacion', 'side', 'high' );
 		add_meta_box( 'informacion_recurso', 'Información Recurso', 'metabox_informacion_recurso', 'recurso', 'side', 'high' );
 
 	});
@@ -22,9 +22,9 @@
 		$direccion 	= get_post_meta($post->ID, '_direccion1_meta', true);
 		$fechas 	= get_post_meta($post->ID, '_fechas_meta', true);
 
-		wp_nonce_field(__FILE__, '_lugar_nonce');
-		wp_nonce_field(__FILE__, '_direccion_nonce');
-		wp_nonce_field(__FILE__, '_fechas_nonce');
+		wp_nonce_field(__FILE__, '_lugar_meta_nonce');
+		wp_nonce_field(__FILE__, '_direccion1_meta_nonce');
+		wp_nonce_field(__FILE__, '_fechas_meta_nonce');
 
 echo <<<END
 
@@ -40,24 +40,24 @@ END;
 	}
 
 	function metabox_informacion_recurso($post){
-		$direccion 		= get_post_meta($post->ID, '_direccion_meta', true);
+		$direccion 		= get_post_meta($post->ID, '_direccion2_meta', true);
 		$telefono 		= get_post_meta($post->ID, '_telefono_meta', true);
 		$email 			= get_post_meta($post->ID, '_email_meta', true);
 		$web 			= get_post_meta($post->ID, '_web_meta', true);
 		$instructables 	= get_post_meta($post->ID, '_instructables_meta', true);
 		$video 			= get_post_meta($post->ID, '_video_meta', true);
 
-		wp_nonce_field(__FILE__, '_direccion_nonce');
-		wp_nonce_field(__FILE__, '_telefono_nonce');
-		wp_nonce_field(__FILE__, '_email_nonce');
-		wp_nonce_field(__FILE__, '_web_nonce');
-		wp_nonce_field(__FILE__, '_instructables_nonce');
-		wp_nonce_field(__FILE__, '_video_nonce');
+		wp_nonce_field(__FILE__, '_direccion2_meta_nonce');
+		wp_nonce_field(__FILE__, '_telefono_meta_nonce');
+		wp_nonce_field(__FILE__, '_email_meta_nonce');
+		wp_nonce_field(__FILE__, '_web_meta_nonce');
+		wp_nonce_field(__FILE__, '_instructables_meta_nonce');
+		wp_nonce_field(__FILE__, '_video_meta_nonce');
 
 echo <<<END
 
 	<label>Dirección:</label>
-	<input type="text" class="widefat" id="direccion" name="_direccion_meta" value="$direccion" />
+	<input type="text" class="widefat" id="direccion" name="_direccion2_meta" value="$direccion" />
 	<label>Teléfono:</label>
 	<input type="text" class="widefat" id="telefono" name="_telefono_meta" value="$telefono" />
 	<label>E-mail:</label>
@@ -109,8 +109,8 @@ END;
 		/*------------------------------------*\
 		    #RECURSO
 		\*------------------------------------*/
-		if ( isset($_POST['_direccion_meta']) and check_admin_referer(__FILE__, '_direccion_meta_nonce') ){
-			update_post_meta($post_id, '_direccion_meta', $_POST['_direccion_meta']);
+		if ( isset($_POST['_direccion2_meta']) and check_admin_referer(__FILE__, '_direccion2_meta_nonce') ){
+			update_post_meta($post_id, '_direccion2_meta', $_POST['_direccion2_meta']);
 		}
 		if ( isset($_POST['_telefono_meta']) and check_admin_referer(__FILE__, '_telefono_meta_nonce') ){
 			update_post_meta($post_id, '_telefono_meta', $_POST['_telefono_meta']);
