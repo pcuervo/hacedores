58522d2e54e559a079f5f890971ca2a11063059b
King Zombie <fmarin@nextlab.org>
2015-01-18 20:57:41 -0600

    fix en js, ocultar campos de link de imagen, agregar validacion para campo categoria cuando esta vacio

--- themes/hacedores/js/admin.js
+++ themes/hacedores/js/admin.js
@@ -1,40 +1,28 @@
-	(function( $ ) {
-		// Uploading files
-		var file_frame;
+var file_frame;	
+(function( $ ) {
+	$('.additional-user-image').on('click', function( event ){
 
-			$('.additional-user-image').on('click', function( event ){
+		event.preventDefault();
+		var txt_area = $(this).prev();
 
-				event.preventDefault();
-				var btn_uploader = $(this);
-				// If the media frame already exists, reopen it.
-				if ( file_frame ) {
-					file_frame.open();
-					return;
-				}
+		// Create the media frame.
+		file_frame = wp.media.frames.file_frame = wp.media({
+			title: $( this ).data( 'uploader_title' ),
+			button: {
+				text: $( this ).data( 'uploader_button_text' ),
+			},
+			multiple: false  // Set to true to allow multiple files to be selected
+		});
 
-				// Create the media frame.
-				file_frame = wp.media.frames.file_frame = wp.media({
-					title: $( this ).data( 'uploader_title' ),
-					button: {
-						text: $( this ).data( 'uploader_button_text' ),
-					},
-					multiple: false  // Set to true to allow multiple files to be selected
-				});
+		// When an image is selected, run a callback.
+		file_frame.on( 'select', function() {
+			attachment = file_frame.state().get('selection').first().toJSON();
+			txt_area.val(attachment.url);
+		});
 
-				// When an image is selected, run a callback.
-				file_frame.on( 'select', function() {
-					// We set multiple to false so only get one image from the uploader
-					attachment = file_frame.state().get('selection').first().toJSON();
-					console.log("algo");
-					console.log(btn_uploader.prev());
-					console.log(attachment.url);
-					// Do something with attachment.id and/or attachment.url here
-					btn_uploader.prev().val(attachment.url);
-				});
-
-				// Finally, open the modal
-				file_frame.open();
-			});
-	}(jQuery));
+		// Finally, open the modal
+		file_frame.open();
+	});
+}(jQuery));
 
 
