bb9667bd30a6ce6d90d23c87c63a6fe7934a50bb
Frnacisco Marin <francisco@synapbox.com>
2015-02-13 18:05:21 -0600

    una columna para post y edicion

--- themes/hacedores/functions.php
+++ themes/hacedores/functions.php
@@ -838,7 +838,18 @@ function addMetaboxEditProyecto(){
 	});
 }
 
+/*Forzamos a que sea solamente una columna*/
+function so_screen_layout_columns( $columns ) {
+    $columns['post'] = 1;
+    return $columns;
+}
+add_filter( 'screen_layout_columns', 'so_screen_layout_columns' );
 
+function so_screen_layout_post() {
+    return 1;
+}
+add_filter( 'get_user_option_screen_layout_post', 'so_screen_layout_post' );
+/*Fin de una columna*/
 
 /**
  * Return an ID of an attachment by searching the database with the file URL.
