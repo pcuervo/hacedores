a8569b2e49a6a05244b47cdce9aa11970fe67b77
raul <raul@zamacona.me>
2015-02-19 12:44:34 -0600

    singles

--- themes/hacedores/sidebar.php
+++ themes/hacedores/sidebar.php
@@ -10,32 +10,29 @@
 			);
 			$queryEvento = new WP_Query( $args );
 			if ( $queryEvento->have_posts() ) : while ( $queryEvento->have_posts() ) : $queryEvento->the_post();
+
 				$eventoDireccion = get_post_meta($post->ID, '_direccion_evento_meta', true);
-				$eventoDia = get_post_meta($post->ID, '_dia_evento_meta', true);
-				$eventoURL = get_post_meta($post->ID, '_web_evento_meta', true);
-				if ( $eventoURL !== '' AND strpos($eventoURL,'http://') === false) {
-					$eventoURL = 'http://'.$eventoURL;
+
+				$diaHora 		= get_post_meta($post->ID, '_dia_hora_evento_meta', true);
+
+				$web 			= get_post_meta($post->ID, '_web_evento_meta', true);
+				if ( $web ){
+					$web = addhttp($web);
 				}
-				$eventoFB = get_post_meta($post->ID, '_facebook_evento_meta', true);
-				$eventoTW = get_post_meta($post->ID, '_twitter_evento_meta', true);
-				if ( $eventoFB !== '' AND strpos($eventoFB,'http://') === false) {
-					$eventoFB = 'http://'.$eventoFB;
-				}
-				if ( $eventoTW !== '' AND strpos($eventoTW,'http://') === false) {
-					$eventoTW = 'http://'.$eventoTW;
+
+				$facebook 		= get_post_meta($post->ID, '_facebook_evento_meta', true);
+				if ( $facebook ){
+					$facebook = addhttp($facebook);
 				}
 			?>
 				<article class="[ margin-bottom-medium ]">
-					<h3><?php the_title(); ?></h3>
-					<p><?php echo $eventoDia; ?></p>
+					<h3><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h3>
+					<p><?php echo $diaHora; ?></p>
 					<p><?php echo $eventoDireccion; ?></p>
-					<p><a href="<?php echo $eventoURL; ?>" target="_blank"><?php echo $eventoURL; ?></a></p>
+					<p><a href="<?php echo $web; ?>" target="_blank"><?php echo $web; ?></a></p>
 					<p class="[ text-center ] [ redes-eventos ]">
-						<?php if ( $eventoFB !== '') { ?>
-							<a href="<?php echo $eventoFB; ?>"><i class="fa fa-facebook fa-2x"></i></a>
-						<?php } ?>
-						<?php if ( $eventoTW !== '') { ?>
-							<a href="<?php echo $eventoTW; ?>"><i class="fa fa-twitter fa-2x"></i></a>
+						<?php if ( $facebook !== '') { ?>
+							<a href="<?php echo $facebook; ?>"><i class="fa fa-facebook fa-2x"></i></a>
 						<?php } ?>
 					</p>
 				</article>
